7ed90af3cfef2b538429551fa2f002f6b3144717
John Paul Villalvazo <33247490+jpvp13@users.noreply.github.com>
2020-11-07 18:53:16 -0800

    Beginnings of Building Database
    Created Database and created the main table that holds all 3000+ tuples

---
+++ RamenQuest_workspace/Data/building.sql
@@ -0,0 +1,15 @@
+-- Creating Table
+
+CREATE TABLE Ramen (
+    ID INT PRIMARY KEY NOT NULL,
+    URL TEXT NOT NULL,
+    Brand VARCHAR NOT NULL,
+    Variety VARCHAR NOT NULL,
+    Style VARCHAR NOT NULL,
+    Country VARCHAR NOT NULL,
+    Stars INT NOT NULL
+);
+
+
+--Importing all tupples
+
