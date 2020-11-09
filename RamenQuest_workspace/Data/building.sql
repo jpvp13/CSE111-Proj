f87fe990d070dc5a4a588174b39a444e7ff0a803
umontes <umontes@ucmerced.edu>
2020-11-08 18:30:08 -0800

    all tables created

--- RamenQuest_workspace/Data/building.sql
+++ RamenQuest_workspace/Data/building.sql
@@ -13,6 +13,35 @@ CREATE TABLE Country_Brand (
     cb_brand TEXT NOT NULL
 );
 
+CREATE TABLE Style (
+   s_style TEXT PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE Users (
+    u_users TEXT PRIMARY KEY NOT NULL,
+    u_userrating INT NULL
+);
+
+CREATE TABLE Variety (
+    v_id TEXT PRIMARY KEY NOT NULL,
+    v_variety TEXT NOT NULL
+);
+
+CREATE TABLE Ramen (
+    r_id INT PRIMARY KEY NOT NULL,
+    r_rating INT NOT NULL,
+    r_url TEXT NOT NULL,
+    r_brand TEXT NOT NULL,
+    r_style TEXT NOT NULL,
+    r_country TEXT NOT NULL,
+    r_users TEXT NOT NULL
+);
+
+CREATE TABLE Style_Brand (
+    sb_brand TEXT PRIMARY KEY NOT NULL,
+    sb_style TEXT NOT NULL
+);
+
 --############################
 --Drop a specified Table
 -- DROP TABLE xxx
